979fb30164f6dcf3e098074b202c9226aa08723e
Ishan Singh <ishan2004.official@gmail.com>
2025-07-14 13:48:24 +0530

    Added slug to quiz

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2022_08_29_042750) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_14_070306) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -62,6 +62,8 @@ ActiveRecord::Schema[7.1].define(version: 2022_08_29_042750) do
     t.uuid "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_quizzes_on_slug", unique: true
     t.index ["user_id"], name: "index_quizzes_on_user_id"
   end
 
