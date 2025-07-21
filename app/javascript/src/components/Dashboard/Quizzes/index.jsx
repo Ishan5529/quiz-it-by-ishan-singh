f374d2e139f8641f49b181a17a5d091383d7dc11
Ishan Singh <ishan2004.official@gmail.com>
2025-07-21 16:47:38 +0530

    Added bulk actions.

--- app/javascript/src/components/Dashboard/Quizzes/index.jsx
+++ app/javascript/src/components/Dashboard/Quizzes/index.jsx
@@ -285,13 +285,48 @@ const Quizzes = () => {
             <Typography className="text-gray-600" style="h4">
               {subHeaderText()}
             </Typography>
-            <Button
-              disabled={!selectedQuizSlugs.length}
-              icon={Delete}
-              label="Delete"
-              size="small"
-              onClick={() => setShowDeleteAlert(true)}
-            />
+            {!isEmpty(selectedQuizSlugs) && (
+              <div className="flex flex-row items-center space-x-2">
+                <Dropdown
+                  buttonStyle="secondary"
+                  label="Change Status"
+                  strategy="fixed"
+                >
+                  <Menu>
+                    <MenuItem.Button
+                      onClick={() => {
+                        handlePublishToggle({
+                          slugs: selectedQuizSlugs,
+                          publishedStatus: true,
+                        });
+                        setSelectedQuizSlugs([]);
+                      }}
+                    >
+                      Draft
+                    </MenuItem.Button>
+                    <MenuItem.Button
+                      onClick={() => {
+                        handlePublishToggle({
+                          slugs: selectedQuizSlugs,
+                          publishedStatus: false,
+                        });
+                        setSelectedQuizSlugs([]);
+                      }}
+                    >
+                      Publish
+                    </MenuItem.Button>
+                  </Menu>
+                </Dropdown>
+                <Button
+                  disabled={!selectedQuizSlugs.length}
+                  icon={Delete}
+                  label="Delete"
+                  size="small"
+                  style="danger"
+                  onClick={() => setShowDeleteAlert(true)}
+                />
+              </div>
+            )}
           </div>
         }
         rightActionBlock={
