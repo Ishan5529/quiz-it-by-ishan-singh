723e71e81b39fa98ce557af69d8078a49966656e
Ishan Singh <ishan2004.official@gmail.com>
2025-07-23 19:52:28 +0530

    Added filter to public page.

--- app/javascript/src/components/Public/Filter.jsx
+++ app/javascript/src/components/Public/Filter.jsx
@@ -1,17 +1,34 @@
 import React from "react";
-import { Typography, Button, Input } from "neetoui";
+import { Dropdown, Input, Typography } from "neetoui";
 import { Search, Filter as FilterIcon } from "neetoicons";
 
-const Filter = () => (
-  <div className="flex w-1/3 flex-row items-center justify-center space-x-4">
-    <Input
-      placeholder="Search for a quiz"
-      prefix={<Search />}
-      onChange={() => {}}
-      size="large"
-    />
-    <FilterIcon />
-  </div>
-);
+const Filter = ({ searchTerm, setSearchTerm, updateQueryParams }) => {
+  const handleChange = ({ target: { value } }) => {
+    setSearchTerm(value);
+    updateQueryParams({ searchTerm: value });
+  };
+
+  return (
+    <div className="flex w-1/3 flex-row items-center justify-center space-x-4">
+      <Input
+        placeholder="Search for a quiz"
+        prefix={<Search />}
+        value={searchTerm}
+        onChange={handleChange}
+        size="large"
+      />
+      <Dropdown
+        buttonStyle="text"
+        size="large"
+        icon={FilterIcon}
+        strategy="fixed"
+      >
+        <div className="p-4">
+          <Typography style="h4">No filters yet.</Typography>
+        </div>
+      </Dropdown>
+    </div>
+  );
+};
 
 export default Filter;
--- app/javascript/src/components/Public/index.jsx
+++ app/javascript/src/components/Public/index.jsx
@@ -1,5 +1,5 @@
 import { routes } from "routes";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 import { Button } from "neetoui";
 import Container from "@bigbinary/neeto-molecules/Container";
@@ -10,15 +10,40 @@ import { usePublicQuizzesFetch } from "hooks/reactQuery/usePublicQuizzesApi";
 import useQueryParams from "hooks/useQueryParams";
 import useFuncDebounce from "hooks/useFuncDebounce";
 import { useAuthState } from "contexts/auth";
+import { filterNonNullAndEmpty } from "utils";
+import { buildUrl } from "utils/url";
+import { useHistory } from "react-router-dom";
 
 import Filter from "./Filter";
 import QuizCard from "./QuizCard";
 
 const Public = () => {
+  const { searchTerm: querySearchTerm = "" } = useQueryParams();
+
+  const [searchTerm, setSearchTerm] = useState(querySearchTerm);
+
+  const params = {
+    title: searchTerm,
+  };
+
   const { isAdmin } = useAuthState();
+  const history = useHistory();
 
   const { data: { data: { quizzes } = {} } = {}, isLoading } =
-    usePublicQuizzesFetch();
+    usePublicQuizzesFetch({
+      title: querySearchTerm,
+    });
+
+  const updateQueryParams = useFuncDebounce(updatedValue => {
+    const updatedParam = {
+      ...params,
+      ...updatedValue,
+    };
+
+    history.push(
+      buildUrl(routes.public.index, filterNonNullAndEmpty(updatedParam))
+    );
+  });
 
   const buttonDestination = isAdmin
     ? routes.dashboard.quizzes.index
@@ -42,7 +67,7 @@ const Public = () => {
         }
       />
       <div className="mb-12 mt-12 flex w-full items-center justify-center">
-        <Filter />
+        <Filter {...{ searchTerm, setSearchTerm, updateQueryParams }} />
       </div>
       <Scrollable className="w-full gap-6 py-6">
         <div className="flex w-full flex-row flex-wrap justify-center gap-8">
